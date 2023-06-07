3a53ed081fd6bbcfd09564bdaad6f835fc040d06
Phat Luong <luongtanphat25@gmail.com>
2023-06-07 19:00:42 -0400

    setup input and ctrl c to terminate

--- play.js
+++ play.js
@@ -2,3 +2,21 @@ const connect = require('./client');
 
 console.log("Connecting...");
 connect();
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on('data', hadnleUserInput);
+  return stdin;
+};
+
+const hadnleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
