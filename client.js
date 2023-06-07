09aaf66d4140c849e74c4ccaeb8165b29e66843f
Phat Luong <luongtanphat25@gmail.com>
2023-06-07 17:59:30 -0400

    created connect function in client.js and call connect in play.js

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+  // conn.on('connect', () => {
+  //   conn.write("Hello from Phat!");
+  // });
+  return conn;
+};
+
+
+
+module.exports = connect;
