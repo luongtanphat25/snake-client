7b2a92a5051e5535b0e6b73e8672b50435e9a7f3
Phat Luong <luongtanphat25@gmail.com>
2023-06-07 20:59:30 -0400

    send message

--- constants.js
+++ constants.js
@@ -4,7 +4,8 @@ const KEYS = {
   '\u0073' : 'Move: down',
   '\u0077' : 'Move: up',
   '\u0061' : 'Move: left',
-  '\u0064' : 'Move: right'
+  '\u0064' : 'Move: right',
+  '\u0020' : 'Say: MSG'
 };
 
 const NAMES = 'Name: PL';
